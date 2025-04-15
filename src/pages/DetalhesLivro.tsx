8d17e6c2a0a7257d8173403b4621a5dc53981ea3
ciraht <167528563+ciraht@users.noreply.github.com>
2025-04-15 11:26:56 -0300

    RESPONSIVO DO LIVRO

--- src/pages/DetalhesLivro.tsx
+++ src/pages/DetalhesLivro.tsx
@@ -335,7 +335,7 @@ const BookDetail = () => {
               <strong>Estoque: </strong> {book.qtd_disponivel}
               </p>
             </div>
-            <div>
+            <div className="d-flex center-x">
               <p className="montserrat-alternates-semibold m-top width-desc">
                 {book.descricao}
               </p>
